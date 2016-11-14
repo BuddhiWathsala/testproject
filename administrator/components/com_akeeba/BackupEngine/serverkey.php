5e594a9dc4854cc13f9023c305d8891dc68f8090
root <root@localhost.localdomain>
2016-11-14 13:38:22 +0530

    first commit

---
+++ administrator/components/com_akeeba/BackupEngine/serverkey.php
@@ -0,0 +1,1 @@
+<?php defined('AKEEBAENGINE') or die(); define('AKEEBA_SERVERKEY', 'ZDczMTlmZmNmNTZhZmM4NWQ3YjJmZTU5Nzk0MTNmZmM='); ?>
---
+++ cache/mod_virtuemart_product/99e17054b06120144548c9540e80f91e-cache-mod_virtuemart_product-0f4a1df9dcdfb7101466a23e90e08bd3.php
@@ -0,0 +1,93 @@
+<?php die("Access Denied"); ?>#x#s:6648:"<div class="vmgroupsolid-2 jsn-icon-display">
+
+	
+		<ul class="vmproductsolid-2 jsn-icon-display productdetails">
+						<li class="product-container  width100 center ">
+				<a href="/shoppingcart/index.php/component/virtuemart/wear/dress-shirt-with-tie-detail?Itemid=0" title="Dress Shirt with tie"><img src="/shoppingcart/components/com_virtuemart/assets/images/vmgeneral/filetype_png.png" alt="VirtueMart Sample" class="featuredProductImage" /></a><div class="clear"></div>				<a href="/shoppingcart/index.php/component/virtuemart/wear/dress-shirt-with-tie-detail?Itemid=0">Dress Shirt with tie</a>        <div class="clear"></div><div class="productdetails"><div class="product-price"><div class="PricesalesPrice vm-display vm-price-value" ><span class="PricesalesPrice" >49,00 €</span></div></div>	<div class="addtocart-area">
+		<form method="post" class="product js-recalculate" action="/shoppingcart/index.php/component/virtuemart/">
+			<div class="vm-customfields-wrap">
+					<div class="product-fields">
+		<div class="product-field product-field-type-A">
+									<span class="product-fields-title-wrapper"><span class="product-fields-title"><strong>Generic Child Variant</strong></span>
+						<span class="hasTooltip" title="&lt;strong&gt;Generic Child Variant&lt;/strong&gt;&lt;br /&gt;Also with tooltip"><img src="/shoppingcart/media/system/images/tooltip.png" alt="Tooltip" /></span></span>
+				<div class="product-field-display"><select name="field[154][412][customfield_value]" size="1" class="vm-chzn-select no-vm-bind avselection" onchange="window.top.location.href=this.options[this.selectedIndex].value" reload=1>
+	<option value="/shoppingcart/index.php/component/virtuemart/dress-shirt-with-tie-detail" selected="selected">Choose a variant</option>
+	<option value="/shoppingcart/index.php/component/virtuemart/wear/dress-shirt-with-tie-s-detail?Itemid=0">DS-Small (49,00 &euro;)</option>
+	<option value="/shoppingcart/index.php/component/virtuemart/wear/dress-shirt-with-tie-l-detail?Itemid=0">DS-Large (49,00 &euro;)</option>
+</select>
+</div>			</div>
+		      <div class="clear"></div>
+	</div>
+	<div class="product-fields">
+		<div class="product-field product-field-type-S">
+									<span class="product-fields-title-wrapper"><span class="product-fields-title"><strong>String, is input</strong></span>
+						<span class="hasTooltip" title="&lt;strong&gt;String, is input&lt;/strong&gt;&lt;br /&gt;Select a variant"><img src="/shoppingcart/media/system/images/tooltip.png" alt="Tooltip" /></span></span>
+				<div class="product-field-display"><select id="customProductData_154_413" name="customProductData[154][13]" class="vm-chzn-select">
+	<option value="414">Gold Buttons +12,00 &euro;</option>
+	<option value="413">Silver Buttons +0,00 &euro;</option>
+</select>
+</div>			</div>
+		      <div class="clear"></div>
+	</div>
+			</div>			
+				
+	<div class="addtocart-bar">
+				<span class="addtocart-button">
+				<span name="addtocart" class="addtocart-button-disabled" title="Choose a variant" >Choose a variant</span>				</span> 			<input type="hidden" name="virtuemart_product_id[]" value="154"/>
+			<noscript><input type="hidden" name="task" value="add"/></noscript> 
+	</div>			<input type="hidden" name="option" value="com_virtuemart"/>
+			<input type="hidden" name="view" value="cart"/>
+			<input type="hidden" name="virtuemart_product_id[]" value="154"/>
+			<input type="hidden" name="pname" value="Dress Shirt with tie"/>
+			<input type="hidden" name="pid" value="154"/>
+			<input type="hidden" name="Itemid" value="103"/>		</form>
+
+	</div>
+
+</div>			</li>
+			
+		</ul><div class="clear"></div>
+		<ul  class="vmproductsolid-2 jsn-icon-display productdetails">			<li class="product-container  width100 center ">
+				<a href="/shoppingcart/index.php/component/virtuemart/product-variants/multi-variant-detail?Itemid=0" title="Multi Variant"><img src="/shoppingcart/components/com_virtuemart/assets/images/vmgeneral/filetype_jpg.png" alt="cart_logo.jpg" class="featuredProductImage" /></a><div class="clear"></div>				<a href="/shoppingcart/index.php/component/virtuemart/product-variants/multi-variant-detail?Itemid=0">Multi Variant</a>        <div class="clear"></div><div class="productdetails"><div class="product-price"><div class="PricesalesPrice vm-display vm-price-value" ><span class="PricesalesPrice" >11,00 €</span></div></div>	<div class="addtocart-area">
+		<form method="post" class="product js-recalculate" action="/shoppingcart/index.php/component/virtuemart/">
+			<div class="vm-customfields-wrap">
+					<div class="product-fields">
+		<div class="product-field product-field-type-C">
+									<span class="product-fields-title-wrapper"><span class="product-fields-title"><strong>Multi Variant</strong></span>
+						<span class="hasTooltip" title="&lt;strong&gt;Multi Variant&lt;/strong&gt;&lt;br /&gt;Also with tooltip"><img src="/shoppingcart/media/system/images/tooltip.png" alt="Tooltip" /></span></span>
+				<div class="product-field-display"><select name="field[300][300][customfield_value][0]" class="vm-chzn-select cvselection no-vm-bind" style="min-width:70px;" reload="1" cvsel="field300">
+	<option value="" selected="selected">-- Select --</option>
+	<option value="46.0000">46.0</option>
+	<option value="51.0000">51.0</option>
+	<option value="56.0000">56.0</option>
+	<option value="61.0000">61.0</option>
+</select>
+<select name="field[300][300][customfield_value][1]" class="vm-chzn-select cvselection no-vm-bind" style="min-width:70px;" reload="1" cvsel="field300">
+	<option value="" selected="selected">-- Select --</option>
+</select>
+<select name="field[300][300][customfield_value][2]" class="vm-chzn-select cvselection no-vm-bind" style="min-width:70px;" reload="1" cvsel="field300">
+	<option value="0" selected="selected">-- Select --</option>
+</select>
+</div>			</div>
+		      <div class="clear"></div>
+	</div>
+			</div>			
+				
+	<div class="addtocart-bar">
+				<span class="addtocart-button">
+				<span name="addtocart" class="addtocart-button-disabled" title="Choose a variant" >Choose a variant</span>				</span> 			<input type="hidden" name="virtuemart_product_id[]" value="300"/>
+			<noscript><input type="hidden" name="task" value="add"/></noscript> 
+	</div>			<input type="hidden" name="option" value="com_virtuemart"/>
+			<input type="hidden" name="view" value="cart"/>
+			<input type="hidden" name="virtuemart_product_id[]" value="300"/>
+			<input type="hidden" name="pname" value="Multi Variant"/>
+			<input type="hidden" name="pid" value="300"/>
+			<input type="hidden" name="Itemid" value="103"/>		</form>
+
+	</div>
+
+</div>			</li>
+					</ul>
+		<div class="clear"></div>
+
+		</div>";
